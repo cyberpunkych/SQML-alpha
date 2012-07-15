b1dc5ccffdaafa1b4a3a05d91af6568f2c23d628
cyber-punk <cyber-punk@xakep.ru>
2012-07-15 22:00:08 +0400

    test files

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python
+#coding: utf8
+__author__ = "cyber-punk"
 
-print 1
+import urllib
+import re
+
+
+
+
+url = "http://m.habrahabr.ru/"
+answ = urllib.urlopen(url)
+answ = answ.read()
+regexp = re.findall('class="t">(.*)</a>', answ)
+regexp2 = re.findall('<a href="(.*)" class="t"', answ)
+
+
+for i in xrange(len(regexp2)):
+	url = regexp2[i]
+	print url
+	answ = urllib.urlopen(url)
+	answ = answ.read()
+	regexp3 = re.findall('<div class="txt">(.*)</div>', answ)
+	print regexp3
