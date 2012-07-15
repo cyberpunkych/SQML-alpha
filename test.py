0c680af1a79cc1954e7cc086f13bf905657594a4
cyberpunkych <cyber-punk@xakep.ru>
2012-07-16 00:55:33 +0400

    Update master

--- test.py
+++ test.py
@@ -5,5 +5,4 @@ page = urllib.urlopen('http://m.habrahabr.ru/post/147682/').read()
 A = html_parser.Parser()
 A.load(page)
 A.query('SELECT WORD @@content, WORD name FROM a')
-print A.value['@@content']
 print A.value['name']
