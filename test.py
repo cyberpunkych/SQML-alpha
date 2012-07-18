1d43de07df3cd0d9db678920653deda33f994ca4
cyberpunkych <cyber-punk@xakep.ru>
2012-07-18 22:31:44 +0400

    update

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env python
-import urllib
-import html_parser
-page = urllib.urlopen('http://m.habrahabr.ru/post/147682/').read()
-A = html_parser.Parser()
-A.load(page)
-A.query('SELECT WORD @@content, WORD name FROM a')
-print A.value['name']
-=======
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import urllib
@@ -17,5 +7,3 @@ A = html_parser.Parser()
 A.load(page)
 A.query("SELECT @title, @@content FROM a WHERE @class=='hub '")
 print A.result
-
->>>>>>> e80a5218d724dc95c5a445f80b52f0bb1960461c
